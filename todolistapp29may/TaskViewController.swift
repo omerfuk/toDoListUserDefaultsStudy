33992edf1af13f06110557c6b808d92c41a924b0
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-05-29 22:45:51 +0300

    .

---
+++ todolistapp29may/TaskViewController.swift
@@ -0,0 +1,33 @@
+//
+//  TaskViewController.swift
+//  todolistapp29may
+//
+//  Created by Ömer Faruk Kılıçaslan on 29.05.2022.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+    @IBOutlet var label:UILabel!
+    
+    var task: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        label.text = task
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Delete", style: .done, target: self, action: #selector(deleteTask))
+    }
+    
+    @objc func deleteTask(){
+//
+//        let newCount = count - 1
+//
+//        UserDefaults().setValue(newCount, forKey: "count")
+//        UserDefaults().setValue(nil, forKey: "task_\(currentPosition)")
+
+    }
+    
+
+
+}
