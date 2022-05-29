c721a01658f8a2410eb3ebef5b4ec2e8268c9d13
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-05-29 21:15:37 +0300

    Initial Commit

---
+++ todolistapp29may/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todolistapp29may
+//
+//  Created by Ömer Faruk Kılıçaslan on 29.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
